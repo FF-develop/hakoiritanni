fe7394f2fc78a1e78e2efd8246ab96ebe4b7185e
FF-develop <98205646+FF-develop@users.noreply.github.com>
2024-01-24 11:10:50 +0900

    Update footer.js

--- common/footer.js
+++ common/footer.js
@@ -1,1 +1,1 @@
-document.write('<p><a href="./copyright.html">© 2021 hakoiritani project</a><br>バージョン:220106-0942<br><a href="http://ffdevelop.php.xdomain.jp/index.html">©FF-Developer_2021</a></p>');
+document.write('<p><a href="./copyright.html">© 2021 hakoiritani project</a><br>バージョン:220106-0942<br><a href="#">©FF-Developer_2021</a></p>');
